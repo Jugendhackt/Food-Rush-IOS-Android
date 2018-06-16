7c97b3966bdc40d4ab1527a6f1b1357e116f1f1b
AdrianStoof <a.stoof@posteo.de>
2018-06-16 19:09:59 +0200

    kA aber es ist gut.

--- app/src/main/java/com/example/calebseeling/foodcrush/Profil.java
+++ app/src/main/java/com/example/calebseeling/foodcrush/Profil.java
@@ -2,12 +2,16 @@ package com.example.calebseeling.foodcrush;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Profil extends AppCompatActivity {
-
+    TextView name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+        name=findViewById(R.id.Name);
+        name.setText("cool");
     }
+
 }
