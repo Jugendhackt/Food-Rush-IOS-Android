0f56e36bad0bffe0ae29a7d99371b8dc9f639b3d
AdrianStoof <a.stoof@posteo.de>
2018-06-16 20:17:56 +0200

    ESSSSSSSSSSSSSSS GEHTTTTTTTTTTTT LOOOOOOOOOOOL

--- app/src/main/java/com/example/calebseeling/foodcrush/MainActivity.java
+++ app/src/main/java/com/example/calebseeling/foodcrush/MainActivity.java
@@ -93,6 +93,7 @@ public class MainActivity extends AppCompatActivity
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+
         navigationView.setNavigationItemSelectedListener(this);
     }
 
@@ -145,7 +146,7 @@ public class MainActivity extends AppCompatActivity
             startActivity(intent);
         }
 
-        item.setChecked(false);
+        item.setCheckable(false);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
