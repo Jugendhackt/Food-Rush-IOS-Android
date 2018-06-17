09c0c838138ad392701110e5492e81b7151820e1
KingC <caleb.seeling@web.de>
2018-06-17 13:17:44 +0200

    Fertig

--- app/src/main/java/com/example/calebseeling/foodcrush/Profil.java
+++ app/src/main/java/com/example/calebseeling/foodcrush/Profil.java
@@ -5,13 +5,12 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class Profil extends AppCompatActivity {
-    TextView name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
-        name=findViewById(R.id.Name);
-        name.setText("cool");
+
     }
 
 }
